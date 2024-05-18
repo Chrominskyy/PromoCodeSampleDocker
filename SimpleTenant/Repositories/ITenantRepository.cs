61f35b7a9391ceaa5f3cf421d25afb337db3c46e
Bartosz Chrominski <bartosz.chrominski@gmail.com>
2024-05-18 13:17:55 +0200

    Removed SimpleAuth, SimpleTenant, will be moved to different repositories

--- SimpleTenant/Repositories/ITenantRepository.cs
+++
@@ -1,9 +0,0 @@
-using Chrominsky.Utils.Repositories.Base;
-using SimpleTenant.Models;
-
-namespace SimpleTenant.Repositories;
-
-public interface ITenantRepository : IBaseDatabaseRepository<Tenant>
-{
-    
-}
--- SimpleTenant/Repositories/ITenantUserRepository.cs
+++
@@ -1,9 +0,0 @@
-using Chrominsky.Utils.Repositories.Base;
-using SimpleTenant.Models;
-
-namespace SimpleTenant.Repositories;
-
-public interface ITenantUserRepository : IBaseDatabaseRepository<TenantUser>
-{
-    
-}
